51fe8a4a21291063d073c1bd642232fb23118323
Prdsilva80 <prdsilva717@gmail.com>
2023-09-28 18:07:39 -0300

    Ajustes na pasta ROOT

--- knexfile.ts
+++ knexfile.ts
@@ -3,13 +3,13 @@ import path from 'path';
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'academia.db'), // Caminho para o arquivo do banco de dados SQLite
+        filename: path.resolve(__dirname, 'src', 'database', 'academia.db'),
     },
     useNullAsDefault: true, // Necessário para o SQLite
     migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations'), // Diretório para armazenar as migrações
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
     },
     seeds: {
-        directory: path.resolve(__dirname, 'src', 'database', 'seeds'), // Diretório para armazenar as sementes de dados
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
     },
 };
